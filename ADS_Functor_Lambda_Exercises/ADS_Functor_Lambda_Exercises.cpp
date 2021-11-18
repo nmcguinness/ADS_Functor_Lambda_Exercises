d6f684fa5e55b7b53e9c5e205729a6b7fa3364a2
Niall McGuinness <niall.mcguinness@dkit.ie>
2021-11-18 09:33:35 +0000

    Added first exercise text

--- ADS_Functor_Lambda_Exercises/ADS_Functor_Lambda_Exercises.cpp
+++ ADS_Functor_Lambda_Exercises/ADS_Functor_Lambda_Exercises.cpp
@@ -21,6 +21,20 @@ int main()
 	Lambda_Exercise2();
 }
 
+/*
+Given the monthly CO2 readings from a city,
+write the code to perform the following:
+
+- Calculate the average CO2.
+- Calculate the standard deviation for all the values that year.
+- Return a list of all values within 10% of the average.
+
+list<int> co2List = {456, 471, 432, 400, 319, 307, 311};
+
+Use any C++ technique(s) that you have learned.
+Avoid using the STL and try to think of your own solution.
+*/
+
 void Functor_Exercise1()
 {
 }
