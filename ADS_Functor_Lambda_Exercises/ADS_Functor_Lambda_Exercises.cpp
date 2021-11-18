754fcc42418ac896fec3ea495f5f9f394048a9a4
Niall McGuinness <niall.mcguinness@dkit.ie>
2021-11-18 09:04:37 +0000

    Add project files.

---
+++ ADS_Functor_Lambda_Exercises/ADS_Functor_Lambda_Exercises.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+void Functor_Exercise1();
+void Functor_Exercise2();
+void Lambda_Exercise1();
+void Lambda_Exercise2();
+
+int main()
+{
+	cout << "Functor_Exercise1" << endl;
+	Functor_Exercise1();
+
+	cout << "Functor_Exercise2" << endl;
+	Functor_Exercise2();
+
+	cout << "Lambda_Exercise1" << endl;
+	Lambda_Exercise1();
+
+	cout << "Lambda_Exercise2" << endl;
+	Lambda_Exercise2();
+}
+
+void Functor_Exercise1()
+{
+}
+
+void Functor_Exercise2()
+{
+}
+
+void Lambda_Exercise1()
+{
+}
+
+void Lambda_Exercise2()
+{
+}
