55d3238aabec87c2a8858506d1142fd007484c42
Niall McGuinness <niall.mcguinness@dkit.ie>
2021-11-18 09:51:12 +0000

    Added exercise 2 and header for the solutions

--- ADS_Functor_Lambda_Exercises/ADS_Functor_Lambda_Exercises.cpp
+++ ADS_Functor_Lambda_Exercises/ADS_Functor_Lambda_Exercises.cpp
@@ -23,26 +23,67 @@ int main()
 }
 
 /*
-Given the monthly CO2 readings from a city,
-write the code to perform the following:
+Given the monthly CO2 readings from a city, write the code to perform the following:
 
-- Calculate the average CO2.
-- Calculate the standard deviation for all the values that year.
-- Return a list of all values within 10% of the average.
+1. Calculate the average CO2.
+2. Calculate the standard deviation for all the values that year.
+3. Return a list of all values within 10% of the average.
 
 list<int> co2List = {456, 471, 432, 400, 319, 307, 311};
 
 Use any C++ technique(s) that you have learned.
 Avoid using the STL and try to think of your own solution.
 */
-
 void Functor_Exercise1()
 {
 	list<int> co2List = { 456, 471, 432, 400, 319, 307, 311 };
+
+	//add your code here...
 }
+
+/*
+You work for a popular bank and are tasked with identifying fraudulent
+deposits related to money laundering. You have been provided with the
+last 10 deposits for a specific bank account.
+
+A deposit record is in the following form:
+
+- DepositRecord (id{string}, amount{double})
+
+1. Write the code to search through a list of 10 deposits and return
+   a list of all deposits where the deposit amount is X% above the average.
+2. Write the code to compare this list of 10 deposits to 10 deposits
+   made in the previous month. The code should return true if the deposits
+   are roughly identical in size. The definition of "roughly" is if each deposit
+   from the new list is +/- X% larger/smaller than the deposit in the same position
+   in the earlier list.
+
+   For example your code will compare the first deposit in the old list
+
+   {id1234, 5000.00}
+
+   with the first deposit in the new list
+
+   {id1234, 15000.00} //new deposit is 3x larger! Fraud/laundering?
+
+Use any C++ technique(s) that you have learned.
+Avoid using the STL and try to think of your own solution.
+*/
+
+struct DepositRecord {
+public:
+	string id;
+	double amount;
+	DepositRecord(string id, double amount) : id(id), amount(amount) {};
+	//add whatever additional operators (e.g. == <, >) and methods (getters) that you need here
+};
 
 void Functor_Exercise2()
 {
+	list<DepositRecord> depositListDec2021
+		= { DepositRecord("id1234", 500) /*add more....*/ };
+
+	//add your code here...
 }
 
 void Lambda_Exercise1()
